a28ca2672fed2c6d002f7e36c6c297475a1bee35
stepanchaparyan <stepanchaparyan@gmail.com>
2019-09-23 22:51:24 +0400

    remove unneccesory packages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'mobx-react';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.scss';
 
 ReactDOM.render(
-    <Provider>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  );
+    <App />,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
 
